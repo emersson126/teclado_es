2c44e5c8552caa54768a8d73a16b036e2a74caab
Emersson Mendoza <emersson126@gmail.com>
2022-02-17 12:26:14 -0500

    teclado_esp

---
+++ js/letras.js
@@ -0,0 +1,32 @@
+var clipboard = new ClipboardJS('.a');
+      var clipboard = new ClipboardJS('.A');
+      
+      var clipboard = new ClipboardJS('.e');
+      var clipboard = new ClipboardJS('.E');
+
+      var clipboard = new ClipboardJS('.i');
+      var clipboard = new ClipboardJS('.I');
+
+      var clipboard = new ClipboardJS('.o');
+      var clipboard = new ClipboardJS('.O');
+
+      var clipboard = new ClipboardJS('.u');
+      var clipboard = new ClipboardJS('.U');
+
+      var clipboard = new ClipboardJS('.ui');
+      var clipboard = new ClipboardJS('.Ui');
+
+      var clipboard = new ClipboardJS('.n');
+      var clipboard = new ClipboardJS('.N');
+
+      var clipboard = new ClipboardJS('.nero1');
+      var clipboard = new ClipboardJS('.nero2');
+      var clipboard = new ClipboardJS('.nero3');
+
+      clipboard.on('success', function (e) {
+        console.log(e);
+      });
+
+      clipboard.on('error', function (e) {
+        console.log(e);
+      });
